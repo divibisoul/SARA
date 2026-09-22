2009e348c41980c234e63222a3506be60200aacd
divibisoul <divibi2011@gmail.com>
2026-09-22 06:22:12 -0300

    feat: export ERU Trinity integration modules

--- src/sara/meta/__init__.py
+++ src/sara/meta/__init__.py
@@ -3,6 +3,9 @@ from .assimilation_committee import AssimilationReviewCommittee
 from .quantum_snapshot import QuantumSnapshotSystem, SnapshotMetadata
 from .eru_engine import ERU_Engine, AuditReport, FrozenState, DiffReport
 from .transystem_sara import TransystemSARA
+from .eru_trinity_bridge import ERUTrinityBridge
+from .eru_drift_detector import ERUDriftDetector
+from .eru_recovery_advisor import ERURecoveryAdvisor
 
 __all__ = [
     "ARAForge", "AdaptedManifest",
@@ -10,4 +13,5 @@ __all__ = [
     "QuantumSnapshotSystem", "SnapshotMetadata",
     "ERU_Engine", "AuditReport", "FrozenState", "DiffReport",
     "TransystemSARA",
+    "ERUTrinityBridge", "ERUDriftDetector", "ERURecoveryAdvisor",
 ]
