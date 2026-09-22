f260864343b6b8edcaf29703f419e9fe50a13988
divibisoul <divibi2011@gmail.com>
2026-09-22 05:58:16 -0300

    fix: preserve audit exports additively

--- src/sara/audit/__init__.py
+++ src/sara/audit/__init__.py
@@ -2,3 +2,4 @@ from .cycle_auditor import CycleAuditor, InvariantResult
 
 __all__ = ["CycleAuditor", "InvariantResult"]
 from .engineering_gate import EngineeringGate, EngineeringGateReport, GateItem
+__all__ += ["EngineeringGate", "EngineeringGateReport", "GateItem"]
