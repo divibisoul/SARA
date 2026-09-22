630e6f17f65a93db9c5082c69cded50c4485ec76
divibisoul <divibi2011@gmail.com>
2026-09-22 08:35:02 -0300

    feat: export working memory from SARA memory package

--- src/sara/memory/__init__.py
+++ src/sara/memory/__init__.py
@@ -1,9 +1,11 @@
 from .dna_tags import DNA_Tags, GuardResult, PROTECTED_TAGS
 from .temporal_vector_db import TemporalVectorDB, Record
 from .regenerative_memory import RegenerativeMemory, VersionRecord
+from .working_memory import WorkingMemory
 
 __all__ = [
     "DNA_Tags", "GuardResult", "PROTECTED_TAGS",
     "TemporalVectorDB", "Record",
     "RegenerativeMemory", "VersionRecord",
+    "WorkingMemory",
 ]
