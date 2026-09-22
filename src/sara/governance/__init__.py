2688985c1ad58e8f6f897a464a0e7fa805e1480d
divibisoul <divibi2011@gmail.com>
2026-09-22 05:44:35 -0300

    fix: restore governance package export compatibility

--- src/sara/governance/__init__.py
+++ src/sara/governance/__init__.py
@@ -2,12 +2,13 @@ from .ubuntu_ethics import UbuntuEthics, FilterResult
 from .buen_vivir import BuenVivir
 from .legal_ai import LegalAI, LegalDecision
 from .legal_compliance import LegalCompliance, ComplianceResult
-from .governed_sara import GovernedSARA, AssimilationReport
+from .governed_sara import GovernedSARA, GovernanceDecision
+from sara.meta.transystem_sara import AssimilationReport
 
 __all__ = [
     "UbuntuEthics", "FilterResult",
     "BuenVivir",
     "LegalAI", "LegalDecision",
     "LegalCompliance", "ComplianceResult",
-    "GovernedSARA", "AssimilationReport",
+    "GovernedSARA", "GovernanceDecision", "AssimilationReport",
 ]
