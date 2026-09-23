bc88830acd06cba9d7e047ced7f9b47430780935
divibisoul <divibi2011@gmail.com>
2026-09-22 23:45:10 -0300

    fix: preserve audit package init unchanged

--- src/sara/audit/__init__.py
+++ src/sara/audit/__init__.py
@@ -1,7 +1,3 @@
 from .cycle_auditor import CycleAuditor, InvariantResult
 
 __all__ = ["CycleAuditor", "InvariantResult"]
-from .engineering_gate import EngineeringGate, EngineeringGateReport, GateItem
-from .engineering_matrix import EngineeringMatrix, MatrixReport, MatrixItem
-
-__all__ += ["EngineeringGate", "EngineeringGateReport", "GateItem", "EngineeringMatrix", "MatrixReport", "MatrixItem"]
