86ffb0b60e66c943836c217c37f4e726885f6b44
divibisoul <divibi2011@gmail.com>
2026-09-22 23:38:28 -0300

    feat: expose integrated engineering gates

--- src/sara/audit/__init__.py
+++ src/sara/audit/__init__.py
@@ -1,3 +1,7 @@
 from .cycle_auditor import CycleAuditor, InvariantResult
 
 __all__ = ["CycleAuditor", "InvariantResult"]
+from .engineering_gate import EngineeringGate, EngineeringGateReport, GateItem
+from .engineering_matrix import EngineeringMatrix, MatrixReport, MatrixItem
+
+__all__ += ["EngineeringGate", "EngineeringGateReport", "GateItem", "EngineeringMatrix", "MatrixReport", "MatrixItem"]
